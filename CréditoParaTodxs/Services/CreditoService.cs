7ed73ec370c1bced256b13ca8d049f8c03666286
AndreHillSantos <andrehillsantos@gmail.com>
2021-06-17 23:20:56 -0300

    Criação da versão de Console.

---
+++ CréditoParaTodxs/Services/CreditoService.cs
@@ -0,0 +1,58 @@
+﻿using Models;
+using System;
+using System.Collections.Generic;
+
+namespace Services
+{
+    public static class CreditoService
+    {
+        public const double RENDA_MINIMA = 1100.0;
+        public const double CREDITO_MINIMO = 1000.0;
+        private const double CREDITO_MAXIMO = 200000.0;
+        private const double COEFICIENTE_CREDITO = 10.0;
+
+        private static readonly int[] _parcelas = { 12, 24, 36 };
+
+        public static bool VerificarValor(string valorInformado, double valorMinimo, double valorMaximo, out double valorRetorno)
+        {
+            if (double.TryParse(valorInformado, out valorRetorno))
+            {
+                return valorRetorno >= valorMinimo && valorRetorno <= valorMaximo;
+            }
+
+            return false;
+        }
+
+        public static bool VerificarRenda(string rendaInformada, out double valorRenda)
+        {
+            return VerificarValor(rendaInformada, RENDA_MINIMA, double.MaxValue, out valorRenda);
+        }
+
+        public static bool VerificarCredito(string creditoInformado, double limiteCredito, out double valorDesejado)
+        {
+            return VerificarValor(creditoInformado, CREDITO_MINIMO, limiteCredito, out valorDesejado);
+        }
+
+        public static double CalcularLimiteCredito(double valorRenda)
+        {
+            if (valorRenda > (CREDITO_MAXIMO / COEFICIENTE_CREDITO))
+            {
+                return CREDITO_MAXIMO;
+            }
+
+            return valorRenda * COEFICIENTE_CREDITO;
+        }
+
+        public static IEnumerable<Proposta> CarregarPropostas(double valorRenda, double valorDesejado)
+        {
+            var propostas = new List<Proposta>();
+
+            foreach (var quantidadeParcelas in _parcelas)
+            {
+                propostas.Add(new Proposta(Array.IndexOf(_parcelas, quantidadeParcelas) + 1, valorDesejado, quantidadeParcelas, valorRenda));
+            }
+
+            return propostas;
+        }
+    }
+}
--- CréditoParaTodxs/ViewConsole/Program.cs
+++ CréditoParaTodxs/ViewConsole/Program.cs
@@ -1,9 +1,79 @@
-﻿using System;
+﻿using Services;
+using System;
+using System.Linq;
 
-namespace ViewConsole {
-    class Program {
-        static void Main(string[] args) {
-            Console.WriteLine("Hello World!");
+namespace ViewConsole
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Bem vindo ao CréditoParaTodxs!\nAqui você poderá realizar um empréstimo pessoal de acordo com a sua saúde financeira.\nQual o valor da sua renda mensal?");
+
+            var rendaInformada = Console.ReadLine();
+            double valorRenda;
+
+            while (!CreditoService.VerificarRenda(rendaInformada, out valorRenda))
+            {
+                Console.WriteLine($"Infelizmente a renda mensal informada não é válida. Por favor, informe um valor válido. (Valor de renda mínima: {CreditoService.RENDA_MINIMA:F2})");
+                rendaInformada = Console.ReadLine();
+            }
+
+            var limiteCredito = CreditoService.CalcularLimiteCredito(valorRenda);
+
+            Console.WriteLine($"Você possui o limite de crédito avaliado em {limiteCredito:F2}.\nQual valor você deseja contratar?");
+
+            var creditoInformado = Console.ReadLine();
+            double valorDesejado;
+
+            while (!CreditoService.VerificarCredito(creditoInformado, limiteCredito, out valorDesejado))
+            {
+                Console.WriteLine($"O valor informado não é válido. O crédito mínimo é de {CreditoService.CREDITO_MINIMO:F2} e máximo é de {limiteCredito:F2}. Favor informar um valor entre os limites.");
+                creditoInformado = Console.ReadLine();
+            }
+
+            var propostas = CreditoService.CarregarPropostas(valorRenda, valorDesejado);
+
+            Console.WriteLine($"Estas são as propostas disponíveis para você. Selecione uma delas através de seu código para saber mais detalhes.");
+            
+            foreach(var proposta in propostas)
+            {
+                Console.WriteLine(proposta.Descricao);
+            }
+
+            Console.WriteLine("Qual proposta você deseja saber mais detalhes?");
+
+            var codigoInformado = Console.ReadLine();
+            double codigoSelecionado;
+
+            while (!CreditoService.VerificarValor(codigoInformado, 1, 3, out codigoSelecionado))
+            {
+                Console.WriteLine($"O código informado não é válido. O código deve ser 1, 2 ou 3.");
+                codigoInformado = Console.ReadLine();
+            }
+
+            var propostaSelecionada = propostas.FirstOrDefault(x => x.Codigo == codigoSelecionado);
+
+            Console.WriteLine("Esta é a proposta selecionada por você.");
+            Console.WriteLine(propostaSelecionada.Detalhes);
+            Console.WriteLine("Deseja realizar a contratação desta proposta? (Digite 'S' para Sim ou 'N' para Não)");
+
+            var propostaAceita = Console.ReadLine().ToUpper();
+
+            while (propostaAceita != "S" && propostaAceita != "N")
+            {
+                Console.WriteLine("Não foi possível entender a confirmação. Por favor, digite 'S' para Sim ou 'N' para Não.");
+                propostaAceita = Console.ReadLine().ToUpper();
+            }
+
+            if (propostaAceita == "S")
+            {
+                Console.WriteLine("Contratação efetuada com sucesso!");
+            } 
+            else
+            {
+                Console.WriteLine("Contratação recusada. Favor reiniciar o processo.");
+            }
         }
     }
 }
