edba782840f3d18ebcbca8e5838b6869df1aa9db
AndreHillSantos <andrehillsantos@gmail.com>
2021-06-20 13:46:08 -0300

    Alterações de projeto.

--- CréditoParaTodxs/ViewForms/Properties/Resources.Designer.cs
+++ CréditoParaTodxs/ViewForms/Properties/Resources.Designer.cs
@@ -8,7 +8,7 @@
 // </auto-generated>
 //------------------------------------------------------------------------------
 
-namespace ViewForms.Properties {
+namespace CréditoParaTodxs.Properties {
     using System;
     
     
@@ -39,7 +39,7 @@ namespace ViewForms.Properties {
         internal static global::System.Resources.ResourceManager ResourceManager {
             get {
                 if (object.ReferenceEquals(resourceMan, null)) {
-                    global::System.Resources.ResourceManager temp = new global::System.Resources.ResourceManager("ViewForms.Properties.Resources", typeof(Resources).Assembly);
+                    global::System.Resources.ResourceManager temp = new global::System.Resources.ResourceManager("CréditoParaTodxs.Properties.Resources", typeof(Resources).Assembly);
                     resourceMan = temp;
                 }
                 return resourceMan;
